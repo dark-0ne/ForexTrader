52f70c1f426256c8ab025a5b8e213994669ad33a
Danial Khoshkholgh <17182126+dark-0ne@users.noreply.github.com>
2020-03-05 03:13:21 +0330

    Remove forex-v0

--- gym_anytrading/__init__.py
+++ gym_anytrading/__init__.py
@@ -3,17 +3,6 @@ from copy import deepcopy
 
 from . import datasets
 
-
-register(
-    id='forex-v0',
-    entry_point='gym_anytrading.envs:ForexEnv',
-    kwargs={
-        'df': deepcopy(datasets.FOREX_EURUSD_1H_ASK),
-        'window_size': 24,
-        'frame_bound': (24, len(datasets.FOREX_EURUSD_1H_ASK))
-    }
-)
-
 register(
     id='forex-v1',
     entry_point='gym_anytrading.envs:MyForexEnv',
